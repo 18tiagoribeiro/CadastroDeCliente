523d0271ec71c032d1d3d96fa1634d5893abce0d
Tiago Ribeiro Nascimento <tiagon@devcenter.com.br>
2025-07-16 17:16:41 -0300

    json serializer

---
+++ Converter/JsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+//namespace CadastroClientesAPI.Converter;
+
+public class DateOnlyJsonConverter : JsonConverter<DateTime>
+{
+    private readonly string _format = "dd/MM/yyyy";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var dateString = reader.GetString();
+        if (DateTime.TryParseExact(dateString, _format, null, System.Globalization.DateTimeStyles.None, out var date))
+        {
+            return date;
+        }
+        throw new JsonException($"Formato de data inválido. Use o formato {_format}.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(_format));
+    }
+}
+
