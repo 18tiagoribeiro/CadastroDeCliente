cb27ce8354717076dd6bcd32f32f00675f30dfe6
Tiago Ribeiro Nascimento <tiagon@devcenter.com.br>
2025-07-18 15:04:56 -0300

    arrumando a hora parte2

--- Controles/ClientesControle.cs
+++ Controles/ClientesControle.cs
@@ -33,7 +33,7 @@ namespace CadastroClientesAPI.Controllers
             }
 
             //mudança no formato da hora
-            cliente.DataNascimento = cliente.DataNascimento.Date;
+            //cliente.DataNascimento = cliente.DataNascimento.Date;
 
             _context.Clientes.Add(cliente);
             await _context.SaveChangesAsync();
