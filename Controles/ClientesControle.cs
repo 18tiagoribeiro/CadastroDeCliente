bda77405519cc52dfd81e09f6e8a820312b0069e
Tiago Ribeiro Nascimento <tiagon@devcenter.com.br>
2025-07-21 14:36:47 -0300

    arrumando nascimento

--- Controles/ClientesControle.cs
+++ Controles/ClientesControle.cs
@@ -33,7 +33,7 @@ namespace CadastroClientesAPI.Controllers
             }
 
             //mudança no formato da hora
-            //cliente.DataNascimento = cliente.DataNascimento.Date;
+            cliente.DataNascimento = cliente.DataNascimento.Date.ToString("dd/MM/yyyy");
 
             _context.Clientes.Add(cliente);
             await _context.SaveChangesAsync();
