6ee454a3cb04d5853c400e6577d719c9602232f0
Tiago Ribeiro Nascimento <tiagon@devcenter.com.br>
2025-07-21 15:38:00 -0300

    ainda na saga da data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,5 +8,15 @@ namespace CadastroClientesAPI.Data
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
 		public DbSet<Cliente> Clientes { get; set; }
-	}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(c => c.DataNascimento)
+                .HasColumnType("date"); // Garante que o campo no banco seja apenas "date"
+        }
+    }
+
 }
